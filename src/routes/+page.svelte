b52bd3f989986f63132ad2b8ffc414d9c75b4d47
Tord Søfteland <tordsofteland@dhcp-10-22-17-65.wlan.ntnu.no>
2024-03-08 15:14:30 +0100

    initial commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,44 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="wrapper">
+<h1>enmo.ai</h1>
+<h4>Moving energy smarter</h4>
+<br>
+<h4>Team:</h4>
+<p>CEO, Johan Vu, <a href="mailto:johan@enmo.ai"> johan@enmo.ai </a> </p>
+<p>Full-stack Developer, David Lekve, <a href="mailto:david@enmo.ai"> david@enmo.ai </a> </p>
+<p>Full-stack Developer, Endre Sørnes, <a href="mailto:endre@enmo.ai"> endre@enmo.ai </a> </p>
+<p>AI Developer, Torjei Helset, <a href="mailto:torjei@enmo.ai"> torjei@enmo.ai </a> </p>
+<p>AI Developer, Simon Vetter, <a href="mailto:torjei@enmo.ai"> simon@enmo.ai </a> </p>
+<p>AI Developer, Farhad Ismayilov, <a href="mailto:farhad@enmo.ai"> farhad@enmo.ai </a> </p>
+<p>CFO, Tord Søfteland, <a href="mailto:tord@enmo.ai"> tord@enmo.ai </a> </p>
+<br>
+<h4>Partners:</h4>
+<p>Berggård Amundsen & Co, <a href="https://baelgros.no" target="_blank"> www.baelgros.no </a> </p>
+
+</div>
+
+
+<style>
+.wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 80vh;
+}
+
+p {
+  margin: 8px;
+  color: #fff;
+  text-align: center;
+}
+
+h4 {
+  margin: 8px;
+  color: #fff;
+  text-align: center;
+}
+
+a {
+  color: #aaa;
+}
+</style>
