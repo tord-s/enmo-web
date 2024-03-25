165d9490c43735b6492c0b840f948b1e93fec779
Tord Søfteland <tordsofteland@dhcp-10-22-18-132.wlan.ntnu.no>
2024-03-25 11:02:17 +0100

    updated titles, favicon and mentors

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,15 +4,19 @@
 <br>
 <h4>Team:</h4>
 <p>CEO, Johan Vu, <a href="mailto:johan@enmo.ai"> johan@enmo.ai </a> </p>
-<p>Full-stack Developer, David Lekve, <a href="mailto:david@enmo.ai"> david@enmo.ai </a> </p>
-<p>Full-stack Developer, Endre Sørnes, <a href="mailto:endre@enmo.ai"> endre@enmo.ai </a> </p>
-<p>AI Developer, Torjei Helset, <a href="mailto:torjei@enmo.ai"> torjei@enmo.ai </a> </p>
-<p>AI Developer, Simon Vetter, <a href="mailto:torjei@enmo.ai"> simon@enmo.ai </a> </p>
-<p>AI Developer, Farhad Ismayilov, <a href="mailto:farhad@enmo.ai"> farhad@enmo.ai </a> </p>
+<p>Fullstack Developer, David Lekve, <a href="mailto:david@enmo.ai"> david@enmo.ai </a> </p>
 <p>CFO, Tord Søfteland, <a href="mailto:tord@enmo.ai"> tord@enmo.ai </a> </p>
+<p>Fullstack Developer, Endre Sørnes, <a href="mailto:endre@enmo.ai"> endre@enmo.ai </a> </p>
+<p>Model Developer, Torjei Helset, <a href="mailto:torjei@enmo.ai"> torjei@enmo.ai </a> </p>
+<p>AI/Fullstack Developer, Simon Vetter, <a href="mailto:torjei@enmo.ai"> simon@enmo.ai </a> </p>
+<p>AI/Fullstack Developer, Farhad Ismayilov, <a href="mailto:farhad@enmo.ai"> farhad@enmo.ai </a> </p>
 <br>
 <h4>Partners:</h4>
 <p>Berggård Amundsen & Co, <a href="https://baelgros.no" target="_blank"> www.baelgros.no </a> </p>
+<br>
+<h4>Mentors:</h4>
+<p>Karen Byskov Lindberg, PhD, <a href="https://www.ntnu.no/ansatte/karen.lindberg" target="_blank"> NTNU </a> </p>
+<p>Steve Völler, PhD, <a href="https://www.ntnu.no/ansatte/steve.voller" target="_blank"> NTNU </a> </p>
 
 </div>
 
