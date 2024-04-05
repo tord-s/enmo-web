39bb3695e8bb8f285c18cffe43b17c80c537f953
Tord Søfteland <tordsofteland@dhcp-10-22-17-243.wlan.ntnu.no>
2024-04-05 10:43:50 +0200

    formatting and text update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,57 +1,91 @@
 <div class="wrapper">
-<h1>enmo.ai</h1>
-<h4>Smarter Energy Movements</h4>
-<br>
-<h4>Team:</h4>
-<p>CEO, Johan Vu, <a href="mailto:johan@enmo.ai"> johan@enmo.ai </a> </p>
-<p>Fullstack Developer, David Lekve, <a href="mailto:david@enmo.ai"> david@enmo.ai </a> </p>
-<p>CFO, Tord Søfteland, <a href="mailto:tord@enmo.ai"> tord@enmo.ai </a> </p>
-<p>Fullstack Developer, Endre Sørnes, <a href="mailto:endre@enmo.ai"> endre@enmo.ai </a> </p>
-<p>Model Developer, Torjei Helset, <a href="mailto:torjei@enmo.ai"> torjei@enmo.ai </a> </p>
-<p>AI / Fullstack Developer, Simon Vetter, <a href="mailto:torjei@enmo.ai"> simon@enmo.ai </a> </p>
-<p>AI / Fullstack Developer, Farhad Ismayilov, <a href="mailto:farhad@enmo.ai"> farhad@enmo.ai </a> </p>
-<br>
-<h4>Partners:</h4>
-<p>Berggård Amundsen & Co, <a href="https://baelgros.no" target="_blank"> www.baelgros.no </a> </p>
-<br>
-<h4>Mentors:</h4>
-<p>Karen Byskov Lindberg, PhD, <a href="https://www.ntnu.no/ansatte/karen.lindberg" target="_blank"> NTNU </a> </p>
-<p>Steve Völler, PhD, <a href="https://www.ntnu.no/ansatte/steve.voller" target="_blank"> NTNU </a> </p>
-
+  <h1>enmo.ai</h1>
+  <h4>Energy Movement</h4>
+  <br />
+  <h4>Team:</h4>
+  <p>CEO, Johan Vu, <a href="mailto:johan@enmo.ai"> johan@enmo.ai </a></p>
+  <p>
+    Fullstack Developer, David Lekve, <a href="mailto:david@enmo.ai">
+      david@enmo.ai
+    </a>
+  </p>
+  <p>CFO, Tord Søfteland, <a href="mailto:tord@enmo.ai"> tord@enmo.ai </a></p>
+  <p>
+    Fullstack Developer, Endre Sørnes, <a href="mailto:endre@enmo.ai">
+      endre@enmo.ai
+    </a>
+  </p>
+  <p>
+    Model Developer, Torjei Helset, <a href="mailto:torjei@enmo.ai">
+      torjei@enmo.ai
+    </a>
+  </p>
+  <p>
+    AI / Fullstack Developer, Simon Vetter, <a href="mailto:torjei@enmo.ai">
+      simon@enmo.ai
+    </a>
+  </p>
+  <p>
+    AI / Fullstack Developer, Farhad Ismayilov, <a href="mailto:farhad@enmo.ai">
+      farhad@enmo.ai
+    </a>
+  </p>
+  <br />
+  <h4>Partners:</h4>
+  <p>
+    Berggård Amundsen & Co, <a href="https://baelgros.no" target="_blank">
+      www.baelgros.no
+    </a>
+  </p>
+  <br />
+  <h4>Mentors:</h4>
+  <p>
+    Karen Byskov Lindberg, PhD, <a
+      href="https://www.ntnu.no/ansatte/karen.lindberg"
+      target="_blank"
+    >
+      NTNU
+    </a>
+  </p>
+  <p>
+    Steve Völler, PhD, <a
+      href="https://www.ntnu.no/ansatte/steve.voller"
+      target="_blank"
+    >
+      NTNU
+    </a>
+  </p>
 </div>
 
-
 <style>
-.wrapper {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  margin-top: 15vh;
-}
-
-p {
-  margin: 8px;
-  color: #fff;
-  text-align: center;
-}
-
-h4 {
-  margin: 8px;
-  color: #fff;
-  text-align: center;
-}
-
-a {
-  color: #aaa;
-}
-
-@media (max-width: 800px) {
   .wrapper {
-    font-size: 13px;
-    line-height: 10px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    margin-top: 15vh;
   }
-}
 
+  p {
+    margin: 8px;
+    color: #fff;
+    text-align: center;
+  }
 
+  h4 {
+    margin: 8px;
+    color: #fff;
+    text-align: center;
+  }
+
+  a {
+    color: #aaa;
+  }
+
+  @media (max-width: 800px) {
+    .wrapper {
+      font-size: 13px;
+      line-height: 10px;
+    }
+  }
 </style>
