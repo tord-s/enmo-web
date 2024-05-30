be9c814b9f4fee81feacaba4a7e5289527002a1e
Tord Søfteland <tordsofteland@dhcp-10-22-99-203.wlan.ntnu.no>
2024-05-30 09:37:45 +0200

    added analytics

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,9 @@
 <script>
   import Lightning from "../lib/Lightning.svelte";
+  import { dev } from "$app/environment";
+  import { inject } from "@vercel/analytics";
+
+  inject({ mode: dev ? "development" : "production" });
 </script>
 
 <Lightning />
